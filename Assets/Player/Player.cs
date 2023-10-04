64fde4df43c1b06da4f942d0eb98fdc9346d12a1
youcheae <kjhstar2486@naver.com>
2023-10-05 01:16:41 +0900

    1005
    코드 다 갈아엎었다!

--- Assets/Player/Player.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
-{
-    public float playerHp = 10;
-    public float playerAttack = 1f;
-}
